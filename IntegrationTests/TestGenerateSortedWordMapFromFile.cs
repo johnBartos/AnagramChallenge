1ce7f022ba6803b4d3cc783bf9e2231db1c5747a
John Bartos <jbartos7@gmail.com>
2015-11-10 22:05:32 -0500

    Add integration test

---
+++ IntegrationTests/TestGenerateSortedWordMapFromFile.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SortedWordMapGenerator;
+using Newtonsoft.Json;
+
+namespace IntegrationTests
+{
+    public class TestGenerateSortedWordMapFromFile
+    {
+        [Test]
+        public void TestGenerateMapFromFile()
+        {
+            var testFilePath = Path.Combine(Directory.GetCurrentDirectory() + @"\testfile.txt");
+
+            var reader = new AnagramFileReader.AnagramFileReader();
+            var mapGenerator = new WordMapGenerator();
+
+            var expected = new Dictionary<string, IList<string>>
+            {
+                {"abt", new List<string> {"bat", "tab"}},
+                {"act", new List<string> {"cat", "act"}}
+            };
+
+            var words = reader.ReadFileIntoMemory(testFilePath);
+
+            var actual = mapGenerator.GenerateSortedWordMap(words);
+
+            Assert.AreEqual(
+             JsonConvert.SerializeObject(expected),
+             JsonConvert.SerializeObject(actual));   
+        }
+    }
+}
