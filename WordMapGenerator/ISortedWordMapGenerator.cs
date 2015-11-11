9c9daf6b600d62102b78e01d6219cd4156c7afa4
John Bartos <jbartos7@gmail.com>
2015-11-10 21:34:53 -0500

    Add class to sort words by char order and place into hashmap, where key is the sorted word and value is list of corresponding unsorted words

---
+++ WordMapGenerator/ISortedWordMapGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordMapGenerator
+{
+    public interface ISortedWordMapGenerator
+    {
+        IDictionary<string, List<string>> GenerateSortedWordMap(IList<string> words);
+    }
+}
