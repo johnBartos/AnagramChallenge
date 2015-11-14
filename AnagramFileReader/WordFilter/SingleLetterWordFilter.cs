3232796f7d70b60e6602813ad270a58ce357b887
John Bartos <jbartos7@gmail.com>
2015-11-13 19:19:10 -0500

    Add another word filter & tests

---
+++ AnagramFileReader/WordFilter/SingleLetterWordFilter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnagramFileReader.WordFilter
+{
+    public class SingleLetterWordFilter : IWordFilter
+    {
+        private List<string> _allowedWords = new List<string> { "a", "i", "o" };
+
+        public bool Pass(string word)
+        {
+            if(word.Length == 1)
+            {
+                foreach (var allowedWord in _allowedWords)
+                {
+                    if (String.Equals(word, allowedWord, StringComparison.OrdinalIgnoreCase))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ UnitTests/WordFilter/TestImpossibleFilter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AnagramChallenge;
+using AnagramFileReader.WordFilter;
+using NUnit.Framework;
+
+namespace UnitTests.WordFilter
+{
+    public class TestImpossibleFilter
+    {
+        [TestCase("cat", "at", true)]
+        [TestCase("cat", "john", false)]
+        public void TestFilter(string anagramString, string word, bool expected)
+        {
+            var anagram = new Anagram(anagramString);
+
+            var filter = new ImpossibleWordFilter(anagram);
+
+            Assert.AreEqual(expected, filter.Pass(word));
+        }
+    }
+}
