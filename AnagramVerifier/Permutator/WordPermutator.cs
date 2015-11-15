e3716dcaa9dc6d591749a258761cb46215e5da89
John Bartos <jbartos7@gmail.com>
2015-11-15 00:50:24 -0500

    Add word permutator & tests

---
+++ AnagramVerifier/Permutator/WordPermutator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnagramVerifier.Permutator
+{
+    public class WordPermutator : IPermutator
+    {
+        
+        public IList<string> Permutate(string sentence)
+        {
+            var results = new List<string>();
+
+            var wordList = sentence.Split(' ');
+
+            Permutate(wordList, 0, wordList.Length - 1, results);
+
+            return results;
+        }
+
+        private void Permutate(IList<string> words, int start, int end, IList<string> results)
+        {
+            if(start == end)
+            {
+                results.Add(String.Join(" ", words));
+            }
+            else
+            {
+                for(var i = start; i <= end; i++)
+                {
+                    Swap(words, start, i);
+                    Permutate(words, start + 1, end, results);
+                    Swap(words, start, i);
+                }
+            }
+        }
+
+        private void Swap(IList<string> words, int first, int second)
+        {
+            var temp = words[first];
+            words[first] = words[second];
+            words[second] = temp;
+        }
+    }
+}
---
+++ UnitTests/Permutator/TestPermutateList.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AnagramVerifier.Permutator;
+using Newtonsoft.Json;
+
+namespace UnitTests.Permutator
+{
+    public class TestPermutateList
+    {
+        [Test]
+        public void TestPermutate()
+        {
+            var words = "a b c";
+
+            var permutator = new WordPermutator();
+
+            var expected = new List<string> {
+                "a b c",
+                "a c b",
+                "b a c",
+                "b c a",
+                "c b a",
+                "c a b"
+            };
+
+            var actual = permutator.Permutate(words);
+
+            Assert.AreEqual(JsonConvert.SerializeObject(expected), JsonConvert.SerializeObject(actual));
+        }
+    }
+}
